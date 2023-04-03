3cfa9d68b1d9ce860b5ccf1ee1f788677ac7004e
Misael Guerrero <mguerrerowest@gmail.com>
2023-04-03 11:51:14 -0400

    create functions to make new projacts and display them on the sidebar

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-projects = [];
+import { projectsArray } from "../../../todoFail/src/projectObjs";
+
+projectsArray = [];
 
 const projectFactory = (name, projectArray) => {
     return {name, projectArray}
@@ -8,4 +10,24 @@ const taskFactory = (name) => {
     return {name}
 };
 
+const inbox = projectFactory('inbox', []);
+projectsArray.push(inbox)
+
+
+function pushProjects() {
+    let project = document.querySelector('#newProject').value
+    const newProject = projectFactory(project, [])
+    projectsArray.push(newProject)
+    displayProjects()
+}
+
+function displayProjects() {
+    const projectsContainer = document.querySelector('.projectsContainer')
+    projectsArray.forEach(element => {
+        let projectDiv = document.createElement('div')
+        projectDiv.className = ('customProject')
+        projectDiv.textContent = element.name
+        projectsContainer.appendChild(projectDiv)
+    });
+}
 
