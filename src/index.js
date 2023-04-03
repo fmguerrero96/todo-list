fedeafce7fd88a120511556e30dd16b4adc1ede6
Misael Guerrero <mguerrerowest@gmail.com>
2023-04-03 11:19:13 -0400

    Create project and tasks factory functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hello')
+projects = [];
+
+const projectFactory = (name, projectArray) => {
+    return {name, projectArray}
+};
+
+const taskFactory = (name) => {
+    return {name}
+};
+
+
