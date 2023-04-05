19c9e0b3de0ebe9728bccf87a36d10e4c15101aa
Misael Guerrero <mguerrerowest@gmail.com>
2023-04-05 15:00:54 -0400

    Create functions to push tasks into their respective project arrays

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ const submitProjectBtn = document.querySelector('#submitProject')
 const projectsContainer = document.querySelector('.projectsContainer')
 const project = document.querySelector('#newProject')
 const projectContent = document.querySelector('.projectContent')
-
+const taskButton = document.querySelector('.taskBtn')
+const taskModal = document.querySelector('.taskModal')
+const submitTaskBtn = document.querySelector('#submitTask')
+const task = document.querySelector('#task')
 
 const projectFactory = (name, projectArray) => {
     return {name, projectArray}
@@ -72,9 +75,51 @@ function createProjectContent(projectName){
     projectContent.appendChild(taskBtn)
 }
 
-document.addEventListener('click', e=> {
+document.addEventListener('click', e=>{
     if (e.target.matches('.customProject')){
         projectContent.innerHTML = ''
         createProjectContent(e.target.textContent)
+        let projectTitle = document.querySelector('.projectTitle').textContent
+        return projectTitle
     }
 })
+
+/* -------------click events on task button---------------------------------- */
+
+document.addEventListener('click', e => {
+    if (e.target.matches('.taskBtn')){
+        taskModal.showModal()
+    }
+})
+
+function findArray(){
+    for (let i = 0; i < projectsArray.length; i++){
+        let projectTitle = document.querySelector('.projectTitle').textContent
+        if(projectsArray[i].name === projectTitle)
+        return projectsArray[i].projectArray
+    }
+}
+
+function pushTasks(array){
+    let task = document.querySelector('#task').value
+    const newTask = taskFactory(task)
+    array.push(newTask)
+    //displayTasks()
+}
+
+submitTaskBtn.addEventListener('click', function(event){
+    event.preventDefault()
+    pushTasks(findArray())
+    taskModal.close()
+    task.value = ''
+    console.log(projectsArray)
+    
+})
+
+/*submitProjectBtn.addEventListener('click', function(event) {
+    event.preventDefault()
+    projectsContainer.innerHTML = ''
+    pushProjects()
+    projectModal.close()
+    project.value = ''
+}) */
